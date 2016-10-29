df8cd7109a8b7f057ef3c03b9163c275d7b03cf0
garethc <gareth@blacksquare.co.za>
2016-10-29 09:01:21 +0200

    intital project setup

---
+++ Jones.Tests/ModelBuilderTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using NSubstitute;
+
+namespace Jones.Tests
+{
+  [TestFixture]
+  public class ModelBuilderTests
+  {
+    [Test]
+    public void TestMethod()
+    {
+      // TODO: Add your test code here
+      Assert.Pass("Your first passing test");
+    }
+  }
+}
