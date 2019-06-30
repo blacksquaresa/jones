d1889c78380025d1979b3c028f0b1423a874a78d
gareth Cornish <gareth@blacksquare.co.za>
2019-06-30 12:22:32 +0200

    Create the beginnings of a compiler in F#

---
+++ Jones.Compiler/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Xml.Linq
+open FSharp.Data
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
